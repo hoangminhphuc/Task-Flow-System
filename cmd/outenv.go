70d87f196816089178b3ae24bc641ed4220c4e3f
hoangminhphuc <22028059@vnu.edu.vn>
2025-04-04 23:18:14 +0700

    Add comment

--- cmd/outenv.go
+++ cmd/outenv.go
@@ -2,6 +2,9 @@ package cmd
 
 import "github.com/spf13/cobra"
 
+// A cli for output all environment variables
+// If build into app.exe then app outenv
+// Run runs the actual command
 var outEnvCmd = &cobra.Command{
     Use:   "outenv",
     Short: "Output all environment variables to std",
